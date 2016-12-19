438f924bca8993d514c94150acf4e3f353f4703e
gaobh <763024185@qq.com>
2016-12-19 18:00:55 +0800

    2016/12/19 crainnogao 后台登初实现录功能

--- src/main/java/com/gao/ssm/module/user/controller/AdminController.java
+++ src/main/java/com/gao/ssm/module/user/controller/AdminController.java
@@ -3,12 +3,14 @@ package com.gao.ssm.module.user.controller;
 import com.gao.ssm.module.user.json.JsonResp;
 import com.gao.ssm.module.user.pojo.BaseUser;
 import com.gao.ssm.module.user.service.UserInfoService;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by 孙皓 on 2016/12/16.
@@ -27,7 +29,14 @@ public class AdminController {
     }
     @RequestMapping(value = "/login" ,method = RequestMethod.POST)
     @ResponseBody
-    public Object login(BaseUser user){
+    public Object login(BaseUser user, @RequestParam(value = "codetext",required = false) String codetext,
+                        HttpServletRequest request){
+
+        HttpSession session = request.getSession();
+        String validateCode = session.getAttribute("validateCode").toString();
+        if (!codetext.equalsIgnoreCase(validateCode)){
+            return new JsonResp(JsonResp.Result_Fail,null,"验证码输入错误",null);
+        }
         BaseUser baseUser = userInfoService.loginValid(user);
         return new JsonResp(JsonResp.Result_Success,null,null,null);
     }
--- src/main/webapp/WEB-INF/statics/js/admin/script.js
+++ src/main/webapp/WEB-INF/statics/js/admin/script.js
@@ -1,23 +1,35 @@
 $(function () {
-    $("#loginSubmit").click(function () {
+    //登录验证
+    $(".submit_lgbtn").click(function () {
         var userName = $("#username").val();
         var password = $("#password").val();
+        var codetext = $("#codetext").val();
         var data = {
             userName:userName,
-            password:password
-        }
+            password:password,
+            codetext:codetext
+        };;;;;;;;;;;;;;;;;;;;;;
         $.ajax({
             type: 'POST',
-            url: "/gaobh/blog/login",
+            url: "/crainnogao/admin/login",
             data: data,
             dataType:"text",
             success: function (resp) {
-                alert("成功");
+                if(resp.result!=null && resp.result==0) {
+                    alert("登录成功");
+                }else{
+                    alert("登录失败");
+                }
             },
             error:function () {
                 alert("失败");
             }
 
         });
+    });;;;;;;;;;;;;;;;;;;;;;
+    //点击刷新图片验证码
+    $(".noclear").click(function () {
+        var time = new Date().getTime();
+       document.getElementById("verPic").src="/VerifyCodeServlet?time="+time;
     })
-})
+});;;;;;;;;;;;;;;;;;;;;;
