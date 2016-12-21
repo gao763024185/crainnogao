7f07584788ca903f471c801e07b6f3636823c085
gaobh <763024185@qq.com>
2016-12-21 18:48:26 +0800

    2016/12/21 crainnogao 后台管理界面初实现

--- src/main/java/com/gao/ssm/module/user/controller/AdminController.java
+++ src/main/java/com/gao/ssm/module/user/controller/AdminController.java
@@ -3,13 +3,14 @@ package com.gao.ssm.module.user.controller;
 import com.gao.ssm.module.user.json.JsonResp;
 import com.gao.ssm.module.user.pojo.BaseUser;
 import com.gao.ssm.module.user.service.UserInfoService;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -22,22 +23,26 @@ public class AdminController {
     private UserInfoService userInfoService;
     String view_base="/admin/";
 
-    /*后台首页*/
+    /*后台登录页*/
     @RequestMapping("/login")
     public String loginPage(){
         return view_base+"login";
     }
     @RequestMapping(value = "/login" ,method = RequestMethod.POST)
     @ResponseBody
-    public Object login(BaseUser user, @RequestParam(value = "codetext",required = false) String codetext,
-                        HttpServletRequest request){
+    public Object login(BaseUser user, @RequestParam(value = "codetext",required = false) String codetext,HttpServletRequest request){
 
         HttpSession session = request.getSession();
         String validateCode = session.getAttribute("validateCode").toString();
-        if (!codetext.equalsIgnoreCase(validateCode)){
+        if (!(codetext.equalsIgnoreCase(validateCode))){
             return new JsonResp(JsonResp.Result_Fail,null,"验证码输入错误",null);
         }
         BaseUser baseUser = userInfoService.loginValid(user);
         return new JsonResp(JsonResp.Result_Success,null,null,null);
+    }
+
+    @RequestMapping("/crainnogao_ad")
+    public String crainnogao_ad(){
+        return view_base+"crainnogao_ad";
     }
 }
