915f9beb08de607fd0f52cdc2b01e8157573b482
gaobh <763024185@qq.com>
2017-02-15 14:01:24 +0800

    2017/02/15

--- src/main/webapp/WEB-INF/statics/js/script.js
+++ src/main/webapp/WEB-INF/statics/js/script.js
@@ -1,60 +1,4 @@
 $(function () {
-    /* 菜单栏 start */
-    // var offset = $("#tab-menu").offset();
-    // var topPadding = 15;
-    // $(window).scroll(function() {
-    //     if ($(window).scrollTop() > offset.top) {
-    //         $("#tab-menu").stop().animate({
-    //             marginTop: $(window).scrollTop() - offset.top + topPadding
-    //         });
-    //     } else {
-    //         $("#tab-menu").stop().animate({
-    //             marginTop: 0
-    //         });
-    //     }
-    // });
-    $('#tab-menu > li').click(function(){
-        $('#tab-menu > li').removeClass('selected');
-        $(this).addClass('selected');
-        $('.tab-content div.tab').slideUp('slow');
-        $('.tab-content div.tab:eq(' + $('#tab-menu > li').index(this) + ')').slideDown('slow');
-    }).mouseover(function() {
-        $(this).addClass('mouseover');
-        $(this).removeClass('mouseout');
-    }).mouseout(function() {
-        $(this).addClass('mouseout');
-        $(this).removeClass('mouseover');
-    });
-
-    // $("#ajshow").on('click','.logTitle',function () {
-    //     hljs.initHighlightingOnLoad();
-    //     var oLogTitle = document.getElementsByClassName('logTitle');
-    //     var _index = $(this).index();
-    //     var logId = $("#logId-"+_index).val();
-    //     var data = {logId:logId};
-    //     $.ajax({
-    //         url:"logshow",
-    //         type:"post",
-    //         data:data,
-    //         dataType:"json",
-    //         success:function (resp) {
-    //             var $tr=  '<div class="panel-heading"><div class="col-lg-12"><span class="col-lg-8"><a href="/crainnogao/detail?logId='+resp.logId+'"><blockquote>'+resp.logTitle+'</blockquote></a></span><span class="col-lg-4">'+resp.logCreatedStr+'</span></div></div>'+
-    //                     '<div  class="contentshow"></div>'+
-    //                     '<div class="panel-footer"><span>关键字：'+resp.logSummary+'</span></div>'+
-    //                     '<input type="hidden" class="logContent" value="'+resp.logContent+'"/>';
-    //             document.getElementById("logshow").innerHTML=$tr;
-    //             $("#firstshow").hide();
-    //             // oLogTitle[_index].css('color','#2a6496');
-    //             var contentshow = $('.logContent').val();
-    //             $('.contentshow').html(marked(contentshow));
-    //
-    //         },
-    //         error:function () {
-    //             alert("失败");
-    //         }
-    //     })
-    // });
-
     $(document).on('click','#crainnogao_ad',function () {
         window.location.href="/crainnogao/admin/crainnogao_ad";
     });
@@ -63,7 +7,8 @@ $(function () {
         var detailcontentshow = $('#detaillogcontentfir').val();
         $('#detaillog').html(marked(detailcontentshow));
     }
-    for (var i=1;i<=5;i++){
+    var count = $('#count').val();
+    for (var i=1;i<=count;i++){
         var contentshow = $('#logcontentfir-'+i).val();
         $('#firstlog-'+i).html(marked(contentshow));
     }
@@ -92,12 +37,14 @@ $(function () {
         }
     }
     $(document).on('click','.prevPage',function () {
-        location.href='/crainnogao/?pageNum='+ --current;
-        current = current+1;
-    });;;;;;;
+        if (current>1){
+            location.href='/crainnogao/?pageNum='+ --current;
+        }
+    });
     $(document).on('click','.nextPage',function () {
-        location.href='/crainnogao/?pageNum='+ ++current;
-        current = current-1;
+        if (current<totalPage){
+            location.href='/crainnogao/?pageNum='+ ++current;
+        }
     })
 });
 
